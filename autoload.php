1f41f362264fb2aad1da579e285a7a01fd76f044
kalenyukk <valters96@gmail.com>
2017-11-08 21:24:50 +0200

    + autoload with composer

--- autoload.php
+++ autoload.php
@@ -1,12 +1,10 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-function autoload($className) {
-    $className = str_replace('App\\', '', $className);
-    $className = str_replace('\\', '/', $className);
-    require_once __DIR__."/src/$className.php";
-}
-
-spl_autoload_register('autoload');
+/* use 'vendor/autoload.php' instead */
+//function autoload($className) {
+//    $className = str_replace('App\\', '', $className);
+//    $className = str_replace('\\', '/', $className);
+//    require_once __DIR__."/src/$className.php";
+//}
+//
+//spl_autoload_register('autoload');

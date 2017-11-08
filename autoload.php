84c13be3e20d18c8d88512c5e42ead2e6876dc59
kalenyukk <valters96@gmail.com>
2017-11-08 20:45:26 +0200

    + improved autoload

--- autoload.php
+++ autoload.php
@@ -4,6 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 function autoload($className) {
+    $className = str_replace('\\', '/', $className);
     require_once __DIR__."/src/$className.php";
 }
 

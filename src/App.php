5f1ad09d9414469ba6fa628440d0074de9a4b6e3
kalenyukk <valters96@gmail.com>
2017-11-08 22:38:11 +0200

    ~ TODOs

--- src/App.php
+++ src/App.php
@@ -32,11 +32,36 @@ class App {
 
             },
             '/sign-up' => function ($request) {
+                // $errors = [];
                 if ($request['info']['REQUEST_METHOD'] == 'POST') {
-//                    User::create($request['params']);
+                    /*
+                     * $rules = [
+                     *    'name' => ['required']
+                     *    'email' => ['required', 'email']
+                     * ];
+                     *
+                     * $errors = App::validate($request['params'], $rules);
+                     *
+                     * For example
+                     *
+                     * $errors = [
+                     *    'name' => 'field is required'
+                     *    'email' => 'is not email'
+                     * ];
+                     *
+                     * if (!$errors) {
+                     *     User::create($request['params']);
+                     * }
+                     *
+                     *
+                     *
+                     * */
                     var_dump($request['params']);
                 }
-                return App::render('signup', ['countries' => Country::all()]);
+                return App::render('signup', [
+                    'countries' => Country::all(),
+                    /* 'errors' => $errors */
+                ]);
             },
             '/sign-in' => function ($request) {
                 return App::render('signin');
